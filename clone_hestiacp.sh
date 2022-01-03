423b0cc736409d69b31c95bcbc4c014ec1c77b1a
Kristan Kenney <94586761+kriskenn@users.noreply.github.com>
2022-01-03 09:00:27 -0400

    Add fork variable for users compiling against their own sources

--- clone_hestiacp.sh
+++ clone_hestiacp.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # Branch to be build from
+fork="hestiacp"
 branch="staging/1.5.3"
 
 # make sure to delete all old  traces
 rm -rf /hestiacp/
 apt-get update && apt-get install git wget curl -y
-git clone https://github.com/hestiacp/hestiacp.git
+git clone https://github.com/$fork/hestiacp.git
 cd ./hestiacp/
 git checkout $branch
