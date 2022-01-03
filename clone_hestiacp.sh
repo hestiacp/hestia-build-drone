785846600940c6808afc479b91f843cd73b82b59
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2022-01-03 13:25:41 +0100

    install wget and curl

--- clone_hestiacp.sh
+++ clone_hestiacp.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # Branch to be build from
+rm -rf /hestiacp/
 branch="staging/1.5.3"
-apt-get update && apt-get install git -y
+apt-get update && apt-get install git wget curl -y
 git clone https://github.com/hestiacp/hestiacp.git
 cd ./hestiacp/
 git checkout $branch
