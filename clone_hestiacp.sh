1793211a4277fb7ac14381c09c2ad897e8197b78
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2022-01-08 11:45:53 +0100

    Install software at this step
    Allowing skipping it in the next step

--- clone_hestiacp.sh
+++ clone_hestiacp.sh
@@ -3,10 +3,11 @@
 # Branch to be build from
 fork="hestiacp"
 branch="staging/1.5.3"
-
+software="build-essential libxml2-dev libz-dev libzip-dev libgmp-dev libcurl4-gnutls-dev unzip openssl libssl-dev pkg-config libsqlite3-dev libonig-dev rpm git wget curl"
 # make sure to delete all old  traces
 rm -rf /hestiacp/
-apt-get update && apt-get install git wget curl -y
+
+apt-get update && apt-get install -y $software 
 git clone https://github.com/$fork/hestiacp.git
 cd ./hestiacp/
 git checkout $branch
