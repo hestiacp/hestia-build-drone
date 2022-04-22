0ba12844457b266f8724b79a353ba5db2f4e47d3
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2022-04-22 04:17:41 +0200

    Update clone_hestiacp.sh

--- clone_hestiacp.sh
+++ clone_hestiacp.sh
@@ -2,7 +2,7 @@
 
 # Branch to build packages from.
 fork="hestiacp"
-branch=" beta/1.6.0"
+branch="beta/1.6.0"
 software="build-essential libxml2-dev libz-dev libzip-dev libgmp-dev libcurl4-gnutls-dev unzip openssl libssl-dev pkg-config libsqlite3-dev libonig-dev rpm git wget curl"
 # make sure to delete all old  traces
 rm -rf /hestiacp/
@@ -11,3 +11,5 @@ apt-get update && apt-get install -y $software
 git clone https://github.com/$fork/hestiacp.git
 cd ./hestiacp/
 git checkout $branch
+
+echo "Start building HestiaCP Packages"
