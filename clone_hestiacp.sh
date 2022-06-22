c8525494c16f54673966274fc6dd12cf746e1522
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2022-06-22 15:22:49 +0200

    Rebuild nginx

--- clone_hestiacp.sh
+++ clone_hestiacp.sh
@@ -2,7 +2,7 @@
 
 # Branch to build packages from.
 fork="hestiacp"
-branch="beta/1.6.0"
+branch="main"
 software="build-essential libxml2-dev libz-dev libzip-dev libgmp-dev libcurl4-gnutls-dev unzip openssl libssl-dev pkg-config libsqlite3-dev libonig-dev rpm git wget curl"
 # make sure to delete all old  traces
 rm -rf /hestiacp/
