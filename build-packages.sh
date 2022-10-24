efbc4d8d442291eb8f1d0292b12c322d36248f85
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2022-10-24 17:29:31 +0200

    Fix issue with rhel9

--- build-packages.sh
+++ build-packages.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 source ./clone_hestiacp.sh
 
+if [ "$all" = 'yes' ]; then
+    ./hestiacp/src/hst_autocompile.sh --dontinstalldeps --all --noinstall --keepbuild --debug '~localsrc'
+    exit 0;
+fi
+
+# For only hestia there are better / faster methods
 if [ "$hestia" = 'yes' ]; then
     ./hestiacp/src/hst_autocompile.sh --dontinstalldeps --hestia --noinstall --keepbuild --debug '~localsrc'
 fi
+
 if [ "$nginx" = 'yes' ]; then
     ./hestiacp/src/hst_autocompile.sh --dontinstalldeps --nginx --noinstall --keepbuild --debug '~localsrc'
 fi
+
 if [ "$php" = 'yes' ]; then
     ./hestiacp/src/hst_autocompile.sh --dontinstalldeps --php --noinstall --keepbuild --debug '~localsrc'
 fi
+
--- clone_hestiacp.sh
+++ clone_hestiacp.sh
@@ -4,6 +4,8 @@
 fork="hestiacp"
 branch="main"
 
+# If need to build all please set all to yes rest to no
+all="no"
 hestia="no"
 nginx="yes"
 php="no"
