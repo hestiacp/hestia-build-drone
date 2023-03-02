3a6c2735b6e756eeef870daa6db893ea67f8d954
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2023-03-02 18:16:13 +0100

    Build Hestia beta2 packages

--- clone_hestiacp.sh
+++ clone_hestiacp.sh
@@ -2,10 +2,10 @@
 
 # Branch to build packages from.
 fork="hestiacp"
-branch="beta/1.7.0"
+branch="staging/1.7.0"
 
 # If need to build all please set all to yes rest to no
-all="no"
+all="yes"
 hestia="no"
-nginx="yes"
+nginx="no"
 php="no"
