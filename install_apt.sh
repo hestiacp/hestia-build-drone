22636c8ccb46afb203e6b886a4aadebc493756ee
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2023-03-05 22:56:38 +0100

    Include lsb-release

--- install_apt.sh
+++ install_apt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source ./clone_hestiacp.sh
 
-software="build-essential libxml2-dev libz-dev libzip-dev libgmp-dev libcurl4-gnutls-dev unzip openssl libssl-dev pkg-config libsqlite3-dev libonig-dev rpm git wget curl"
+software="build-essential libxml2-dev libz-dev libzip-dev libgmp-dev libcurl4-gnutls-dev unzip openssl libssl-dev pkg-config libsqlite3-dev libonig-dev rpm git wget curl lsb-release"
 apt-get update && apt-get install -y $software 
 
 # make sure to delete all old  traces
