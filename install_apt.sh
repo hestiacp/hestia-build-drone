7242d1796b2efa023aaac99962866c9563033a65
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2023-05-05 19:33:30 +0200

    Rewrite Building packages
    Promote the last build:
    Set: Branch: staging/1.x.x or main 
    Promote
    
    
    Use alpine/git
    
    
    Go into folder
    
    
    test
    
    
    test
    
    
    Make sure there is something to test
    
    
    rename test
    
    
    Restore tests

--- install_apt.sh
+++ install_apt.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-source ./clone_hestiacp.sh
-
 software="build-essential libxml2-dev libz-dev libzip-dev libgmp-dev libcurl4-gnutls-dev unzip openssl libssl-dev pkg-config libsqlite3-dev libonig-dev rpm git wget curl lsb-release"
 apt-get update && apt-get install -y $software 
 
